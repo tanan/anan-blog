20f79b58d47973adc8289e2ffda506962a22d509
toshifumi.anan <toshifumi.anan@gmail.com>
2021-09-28 15:18:32 +0900

    add new web apps

---
+++ apps/blog-web/src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <MainLogo />
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import MainLogo from '@/components/atoms/MainLogo.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld,
+    MainLogo
+  }
+}
+</script>
