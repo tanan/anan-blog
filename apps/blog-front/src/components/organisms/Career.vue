d0c6ce086fda8b8c861b78caf0bb3e7bd7316748
toshifumi.anan <toshifumi.anan@gmail.com>
2021-12-28 23:06:47 +0900

    add master career

--- apps/blog-front/src/components/organisms/Career.vue
+++ apps/blog-front/src/components/organisms/Career.vue
@@ -17,6 +17,17 @@ export default {
       careers: [
         {
           id: '1',
+          period: '2021年10月 - 現在',
+          company: {
+            name: '静岡大学大学院',
+            url: 'https://www.shizuoka.ac.jp/'
+          },
+          role: '総合科学研究科情報学専攻修士課程',
+          description: '量子情報科学分野の研究。現在は主に関節制御やエラー軽減等の研究を行っている',
+          technology: 'python, Qiskit, Qulacs'
+        },
+        {
+          id: '2',
           period: '2016年7月 - 現在',
           company: {
             name: '株式会社ユーザベース',
@@ -27,7 +38,7 @@ export default {
           technology: 'on-premises, Xen Server, Proxmox, AWS, GCP, Linux, Docker, Kubernetes, Istio, MySQL, PostgreSQL, Elasticsearch, Aerospike, Apache, Nginx, Squid, LVS, Keepalived, HAProxy, Fluentd, Unbound, Ansible, Terraform, Hinemos, Jenkins, Buildkite, ArgoCD, Prometheus, Nagios, Cacti, Golang, Python, Java, Kotlin, Angular Dart'
         },
         {
-          id: '2',
+          id: '3',
           period: '2012年4月 - 2016年6月',
           company: {
             name: '京セラコミニュケーションシステム株式会社',
@@ -38,7 +49,7 @@ export default {
           technology: 'on-premises, AWS, C#, Java, Ansible, Windows Server 2008/2012, HyperV, IIS, Aerospike, MySQL, SQLServer, Fluentd, Treasure Data'
         },
         {
-          id: '3',
+          id: '4',
           period: '2008年4月 - 2012年3月',
           company: {
             name: '熊本大学',
@@ -49,7 +60,7 @@ export default {
           technology: 'Fortran 90'
         },
         {
-          id: '4',
+          id: '5',
           period: '2005年4月 - 2008年3月',
           company: {
             name: '大分県立竹田高等学校',
