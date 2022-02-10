08909cdcb68a8bf4613e2cbb147157b3a38f34ca
toshifumi.anan <toshifumi.anan@gmail.com>
2022-02-10 16:16:15 +0900

    health-planet-api: create entrypoint

---
+++ apps/health-planet-api/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"flag"
+	"health-planet-api/config"
+	"health-planet-api/infrastracture/web"
+)
+
+var (
+	path = flag.String("c", "config.localhost.yaml", "config file path")
+)
+
+func main() {
+	flag.Parse()
+
+	con, err := config.LoadFile(*path)
+	if err != nil {
+		panic(err)
+	}
+
+	con.LoadEnvPassword()
+	web.LoadRouter(con).Start(":10010")
+}
